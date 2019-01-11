2798636dac2d2821e147ff46b791303543d9455a
Nathanael Beisiegel <pknb.dev@gmail.com>
2019-01-11 15:43:54 -0500

    Fixed a couple examples and added index

--- badScriptOnClick.js
+++ badScriptOnClick.js
@@ -1,3 +1,3 @@
-document.getElementById("#button").addEventListener("click", function() {
+document.getElementById("button").addEventListener("click", function() {
   throw new Error("Error on click");
 });
